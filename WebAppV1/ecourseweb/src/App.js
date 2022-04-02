353967311a4b50ec0351eae9776ba2df244a6ae9
VoMinhHung-SR <vominhhung1546@gmail.com>
2022-04-03 00:07:37 +0700

    demo

---
+++ WebAppV1/ecourseweb/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Lesson from './components/Lesson';
+import Header from './layout/Header';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses/:courseId/lessons" element={<Lesson />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
